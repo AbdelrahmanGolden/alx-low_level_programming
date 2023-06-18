cd31de906e6a3c8b0df395e48c637896c2e5bb06
AbdelrahmanGolden <abdelrahmanabdelkaliq955@gmail.com>
2023-06-18 07:50:58 -0700

    p

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 /**
  * main - Entry point
  *
@@ -11,6 +10,6 @@
 
 int main(void)
 {
-	puts "\"Programming is like building a multilingual puzzle";
+	puts ("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
