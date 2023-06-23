133419da67989e9d59751760294550701353ceb9
AbdelrahmanGolden <abdelrahmanabdelkaliq955@gmail.com>
2023-06-23 10:39:51 -0700

    a

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ *
+ * Return: always 0
+*/
+
+void print_numbers(void)
+{
+	int n = 0;
+
+	do {
+		_putchar(n + 48);
+	} while (n >= 0 && n <= 9);
+	_putchar('\n');
+}
