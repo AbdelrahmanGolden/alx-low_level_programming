2236fd386d5d903bee8a964e24b459c46cca0bfd
AbdelrahmanGolden <abdelrahmanabdelkaliq955@gmail.com>
2023-06-26 16:56:11 -0700

    a

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - the length of a string
+ *
+ * @s: string
+ *
+ * Return: the lenght of the string
+*/
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; *s != '\0'; s++)
+		++i;
+
+	return (i);
+}
